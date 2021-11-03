277ba5b96a1ab420e8be73f29052734e79952f74
Peter Olu <peter.o.olu@gmail.com>
2021-11-03 22:26:08 +0100

    app init nav done

---
+++ pages/auth/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-content>
+    <v-container class="fill-height" fluid>
+      <v-row align="center">
+        <v-col cols="12" sm="8" md="8">
+          <v-card> </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+export default {
+  layout: 'session',
+}
+</script>
+
+<style></style>
