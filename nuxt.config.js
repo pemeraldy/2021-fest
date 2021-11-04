56e50bdb7333a609e67a99709f1d3b9b641e1218
Peter Olu <peter.o.olu@gmail.com>
2021-11-04 04:18:17 +0100

    generate deply

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,9 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
+    // Deploy setting
+    target: 'static',
+    // ssr: false, //if client side only
     titleTemplate: '%s - Petra',
     title: 'PetraCC',
     htmlAttrs: {
