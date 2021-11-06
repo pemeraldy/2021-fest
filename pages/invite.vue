fe8770ca12cc9bc31392cc9b0ad5f506dba8e42b
Peter Olu <peter.o.olu@gmail.com>
2021-11-06 08:50:43 +0100

    added page image bg

--- pages/invite.vue
+++ pages/invite.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <AppHero background-image="invite-hero.png" />
+    <v-container class="mt-12">
+      <v-row>
+        <v-col cols="12">
+          <div></div>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
